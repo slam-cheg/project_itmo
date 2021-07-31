45f3912fc806b45cf94839139e284e2ff8df54b2
eilerglen <83466883+eilerglen@users.noreply.github.com>
2021-07-31 15:42:26 +0300

    header 80%, main block 90%, about (no js ), publucations(no js)

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 /*Открытие навигационного меню */ 
 let burgerMenuButton = document.querySelector('.header__button-burger');
 let burgerMenu = document.querySelector('.header__nav-bar');
+let logoHeader = document.querySelector('.logo_place_header');
 
 function openMenu(burgerMenu) {
-    burgerMenu.classList.add('header__nav-bar_active')
+    burgerMenu.classList.add('header__nav-bar_active');
+    logoHeader.classList.add('logo_active');
+    burgerMenuButton.classList.add('header__button-burger_active');
   }
 
 burgerMenuButton.addEventListener('click', () => {
@@ -16,7 +19,9 @@ let navBarMobileClose = document.querySelector('.header__button-close');
 
 function closeMenu(burgerMenu) {
     burgerMenu.classList.remove("header__nav-bar_active");
-   }
+    logoHeader.classList.remove('logo_active');
+    burgerMenuButton.classList.remove('header__button-burger_active');
+  }
 
    navBarMobileClose.addEventListener('click', () => {
     closeMenu(burgerMenu)
